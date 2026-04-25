1aae850d782f65e76ebfb48bcbb65e347a4b82ff
Dayan <dayanulanov.wer@gmail.com>
2026-04-25 23:18:43 +0600

    feat: add new method intersection and update command

--- src/utils/pull.ts
+++ src/utils/pull.ts
@@ -2,7 +2,6 @@
 * @description Removes all specified values ​​from the array. Uses Set for high performance.
 * @tags array, filter, transformation
 * @category array
-* @dependency none
 */
 export function pull<T>(arr: T[], ...removeList: T[]): T[] {
   const removeSet = new Set(removeList);
